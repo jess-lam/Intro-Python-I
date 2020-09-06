b8c7bca7aebd4e7c462c06132884532fd43619e2
Jessica Lam <jemlam93@gmail.com>
2020-09-06 15:57:28 -0700

    completed slices and comprehensions

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -11,6 +11,8 @@ for more info regarding list comprehensions.
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
 y = []
+for x in range(5):
+    y.append(x+1)
 
 print (y)
 
@@ -18,6 +20,8 @@ print (y)
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
 y = []
+for x in range(10):
+    y.append(x**3)
 
 print(y)
 
@@ -28,6 +32,9 @@ a = ["foo", "bar", "baz"]
 
 y = []
 
+for i in range(0, len(a)):
+    y.append(a[i].upper())
+
 print(y)
 
 # Use a list comprehension to create a list containing only the _even_ elements
@@ -36,6 +43,6 @@ print(y)
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
+y = [num for num in x if int(num) % 2 == 0]
 
 print(y)
